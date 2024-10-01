bd7c370a23649da301acacea7cd73ef739dd42ca
Brandon Yang <bclyang@gmail.com>
2024-10-01 05:32:11 -0700

    merged Add context parallelism support

--- megatron/neox_arguments/neox_args.py
+++ megatron/neox_arguments/neox_args.py
@@ -95,21 +95,17 @@ class NeoXArgsParallelism(NeoXArgsTemplate):
     """
     flag to determine whether Megatron-style Sequence Parallelism (https://arxiv.org/abs/2205.05198)
     (Layernorm inputs and activations are sharded across model parallel group) will be used. Has no effect when model_parallel_size is 1.
-<<<<<<< HEAD
-=======
     """
 
     is_context_parallel: bool = False
     """
     flag to determine whether context parallelism is on - shouldn't be set by user, is automatically determined
     according to context parallel size.
->>>>>>> 4969683e (Add context parallelism support)
     """
 
-    is_context_parallel: bool = False
+    expert_interval: int = 2
     """
-    flag to determine whether context parallelism is on - shouldn't be set by user, is automatically determined
-    according to context parallel size.
+    Have one MoE layer every expert_interval layers
     """
 
 
